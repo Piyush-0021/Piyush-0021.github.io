ff4b505f86280f77e2962bd3feaa4d31cbcf8429
Piyush-0021 <guptapiyush2105@gmail.com>
2025-07-04 20:11:29 +0530

    Update supabase.ts

--- src/app/lib/supabase.ts
+++ src/app/lib/supabase.ts
@@ -1,14 +1,3 @@
-// src/lib/supabase.ts
-// import { createClient } from '@supabase/supabase-js';
-
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// if (!supabaseUrl || !supabaseAnonKey) {
-//   throw new Error('❌ Missing Supabase credentials');
-// }
-
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
 // lib/supabase.ts
